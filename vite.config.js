acbc4e8e1c6a0ad4b8619a0a4e1b2e3b5cc3e589
靳成飞 <12050837+jin-chengfei@user.noreply.gitee.com>
2024-09-28 17:44:38 +0800

    test 2

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,17 @@ export default defineConfig({
     alias: {
       "@": resolve(__dirname, "./src")
     }
-  }
+  },
+  server: {
+      proxy: {
+        // 匹配所有以 '/v3pz' 开头的请求
+        '/v3pz': {
+          target: 'https://v3pz.itndedu.com/', // 目标服务器
+          changeOrigin: true, // 推荐开启
+          rewrite: (path) => path.replace(/^\/v3pz/, '/v3pz'), // 重写路径
+        }, 
+  
+      },
+    },
 
 })
