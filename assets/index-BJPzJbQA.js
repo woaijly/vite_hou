ec3b907260446db835148e7dd4c789ad29613e83
靳成飞 <12050837+jin-chengfei@user.noreply.gitee.com>
2024-09-28 19:09:12 +0800

    提交修改地址

--- assets/index-BJPzJbQA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,d as c,e as n}from"./index-BTl7R4UL.js";const r={};function t(o,s){return c(),n("div",null,"控制台")}const a=e(r,[["render",t]]);export{a as default};
---
+++ assets/index-D_SS9Bgn.js
@@ -0,0 +1,1 @@
+import{_ as e,d as c,e as n}from"./index-CHJFmuEZ.js";const r={};function t(o,s){return c(),n("div",null,"控制台")}const a=e(r,[["render",t]]);export{a as default};
